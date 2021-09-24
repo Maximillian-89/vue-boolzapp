b4d46668c8bd5d9825bdbcaac3177b9f85982ef8
Maximillian-89 <massimiliano.pignataro@gmail.com>
2021-09-24 11:25:49 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -9,17 +9,17 @@ const app = new Vue (
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '15:30',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Ricordati di dargli da mangiare',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 16:15:22',
+                        date: '16:15',
                         message: 'Tutto fatto!',
                         status: 'received'
                     }
@@ -30,17 +30,17 @@ const app = new Vue (
                 avatar: '_2',
                 visible: true,
                 messages: [{
-                    date: '20/03/2020 16:30:00',
+                    date: '16:30',
                     message: 'Ciao come stai?',
                     status: 'sent'
                 },
                     {
-                        date: '20/03/2020 16:30:55',
+                        date: '16:30',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
-                        date: '20/03/2020 16:35:00',
+                        date: '16:35',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'received'
                     }
@@ -51,17 +51,17 @@ const app = new Vue (
                 avatar: '_3',
                 visible: true,
                 messages: [{
-                    date: '28/03/2020 10:10:40',
+                    date: '10:10',
                     message: 'La Marianna va in campagna',
                     status: 'received'
                 },
                     {
-                        date: '28/03/2020 10:20:10',
+                        date: '210:20',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
-                        date: '28/03/2020 16:15:22',
+                        date: '16:15',
                         message: 'Ah scusa!',
                         status: 'received'
                     }
@@ -72,12 +72,12 @@ const app = new Vue (
                 avatar: '_4',
                 visible: true,
                 messages: [{
-                    date: '10/01/2020 15:30:55',
+                    date: '15:30',
                     message: 'Lo sai che ha aperto una nuova pizzeria?',
                     status: 'sent'
                 },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '15:50',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
                     }
